bfd041a1528a75aac695270ac4e04b1e663757e3
Nutchy <nutchy@loomx.io>
2020-02-12 02:15:29 +0700

    simple required login

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,18 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 
+import store from '../store'
+
 Vue.use(VueRouter)
+
+const requiredAuth = (to, from, next) => {
+  const isLoggedIn = store.getters['auth/isLoggedIn']
+  if (isLoggedIn) {
+    next()
+    return
+  }
+  next({ name: 'Login' })
+}
 
 const routes = [
   {
@@ -13,10 +24,16 @@ const routes = [
   {
     path: '/about',
     name: 'About',
+    beforeEnter: requiredAuth,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/Login.vue')
   }
 ]
 
---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Login page</h1>
+    <button @click="login">Login</button>
+    {{isLoggedIn}}
+  </div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex'
+const authStore = createNamespacedHelpers('auth')
+export default {
+  methods: {
+    ...authStore.mapActions(['login'])
+  },
+  computed: {
+    ...authStore.mapGetters(['isLoggedIn'])
+  },
+  watch: {
+    isLoggedIn: function(val) {
+      val && this.$router.push({ name: 'About'})
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
